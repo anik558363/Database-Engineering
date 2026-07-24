d3a6ac8a5f4cb71352f85386d705c87bc75ff20d
Anik Mondol <anik(558363)@gmail.com>
2026-07-24 20:36:24 +0600

    Update example_table schema and add demo_table creation script

--- class-11.sql
+++ class-11.sql
@@ -1,14 +1,14 @@
 CREATE Table example_table(
 
-    id SERIAL PRIMARY KEY,    ----- SERIAL(int 2.1b), BITSERIAL(64-BIT)
+    id BIGSERIAL PRIMARY KEY,    ----- SERIAL(int 2.1b), BIGSERIAL(64-BIT)
     col1 BIGINT,   ----- INTEGER(2.1b), smallint(32k), bigint(larg int)
     col2 NUMERIC(10, 2),  ---- NUMERIC, DECIMAL Financial data
-    col3 DOUBLE (10, 4), --- DOUBLE, REAL
+    col3 NUMERIC (10, 4), --- DOUBLE, REAL
     col4 TEXT,  --- CHAR(255), VARCHAR(100), TEXT
     col5 BOOLEAN, --- BOOLEAN true, false
     col6 TIME,   --- DATE only, TIME only, TIMESTAMP(data + time)
     col7 JSON,  --- JSON (json text), JSONB (binary JSON text)
-    col8 array, --- ARRAY TEXT
-    col9 UUID, UNIQUE number
+    col8 TEXT[], --- ARRAY TEXT
+    col9 UUID  -- UNIQUE number
 
 );
--- class-13.sql
+++ class-13.sql
@@ -2,13 +2,13 @@ CREATE Table demo_table (
     id SERIAL PRIMARY KEY, --- PRIMARY KEY + AUTO increment
     full_name VARCHAR(50) NOT NULL, --- Required field Validtion 
     email VARCHAR(50) UNIQUE NOT NULL,
-    UNIQUE field validtion phone VARCHAR(11),
+   --- UNIQUE field validtion phone VARCHAR(11),
     age INT CHECK (age >= 18), --- validtion Condition
     is_active BOOLEAN DEFAULT TRUE, --- DEFAULT value
-    city VARCHAR(50) DEFAULT "Dhaka", --- DEFAULT Dhaka
+    city VARCHAR(50) DEFAULT 'Dhaka', --- DEFAULT Dhaka
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, --- Current Time & DATE Auto Insert
     profile1 JSONB DEFAULT '{}'::JSONB, --- JSONB DEFAULT
     profile2 JSON DEFAULT '{}'::JSON, --- JSON DEFAULT
     dress_color TEXT[] DEFAULT ARRAY[]::TEXT[], --- DEFAULT []
-    unique_number UUID DEFAULT gen_random_uuid UNIQUE,
-)
+    unique_number UUID DEFAULT gen_random_uuid() UNIQUE
+);

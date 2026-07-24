5e423287d36f46d6d2bc85be0723f6966af0071f
Anik Mondol <anik(558363)@gmail.com>
2026-07-24 21:09:02 +0600

    Add SQL script for employee table operations including inserts, updates, deletes, and truncation

---
+++ class-15.sql
@@ -0,0 +1,35 @@
+
+
+-- INSERT INTO employess (full_name, salary, date_of_joining) VALUES ( 'Emon Mondal', 22000, '2026-01-31');   ----- single value insert
+
+
+
+-- INSERT INTO employess (full_name, salary, date_of_joining) VALUES ( 'Arpon Mondal', 19000, '2026-01-31'),( 'Apu Mondal', 19000, '2026-07-31'), ( 'Emon Mondal', 18000, '2026-05-31');   ----- multiple value insert
+
+
+
+-- DELETE FROM employess WHERE full_name = 'Apu Mondal';
+
+
+-- SELECT * FROM employess;
+
+-- SELECT id, date_of_joining, full_name FROM employess 
+
+-- SELECT * FROM employess WHERE salary <= 19000
+
+
+-- UPDATE employess SET salary = 25000 WHERE full_name = 'Anik Mondal';
+
+-- UPDATE employess set salary = 30000 WHERE id = 2;
+
+
+-- DELETE FROM employess WHERE id = 1
+
+
+-- SELECT FROM employess WHERE id = 2
+
+
+-- DELETE FROM employess
+
+
+TRUNCATE Table employess

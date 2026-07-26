1e422168bb648e594c52333ef967616e538fcc96
Anik Mondol <anik(558363)@gmail.com>
2026-07-27 01:22:52 +0600

    Add SQL script for user, customer, category, product, invoice, and sales management

---
+++ class-23.sql
@@ -0,0 +1,45 @@
+user management:
+
+1. registration, profile, password manage, login
+
+
+customer management:
+
+1. users can add, udatate, delete, and view their customer details
+2. customer information should include name, email and phone number.
+
+
+
+category management:
+
+1. users can add, udatate, delete, and view their category details
+2. category name
+
+
+
+product management:
+
+1. users must be able to create, update, delete and view their product catelog
+2. product should be organized under categorires
+3. category, name, price, unite, iamge
+
+
+
+
+
+invoice management:
+
+1. users should be able to generate invoices for customers
+2. each invoice should display details such as total amount, discounts, vat and the final payable amount
+
+
+
+invoice products management:
+
+1. users must be able to add products to an invoice with quentities and sale prices
+
+
+
+sales reporting:
+
+1. data ranage sales report generate

0341bdfdf00cd0b14d62ef2cd9ec25084e6a15e4
Anik Mondol <anik(558363)@gmail.com>
2026-08-02 10:30:53 +0600

    Create initial database schema for sales_db with users, categories, customers, products, invoices, and invoice_products tables

---
+++ class-29.sql
@@ -0,0 +1,100 @@
+CREATE Table users (
+    
+
+    id BIGSERIAL PRIMARY KEY,
+    firstName VARCHAR(50) NOT NULL,
+    lastName VARCHAR(50) NOT NULL,
+    email VARCHAR(50) UNIQUE NOT NULL,
+    mobile VARCHAR(15) NOT NULL,
+    pass VARCHAR(500) NOT NULL,
+    otp VARCHAR(10) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    
+    
+    );
+
+
+
+
+
+    -- user_id BIGINT == id BIGINT
+    -- FOREIGN KEY (user_id)
+    -- FOREIGN users(id)
+
+
+
+CREATE Table categories (
+    
+
+    
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    -- user_id ----> categories(user_id) ---> users(id)
+    user_id BIGINT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE RESTRICT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
+    
+    );
+
+CREATE Table customers (
+    
+    
+    id BIGSERIAL PRIMARY KEY,
+
+
+
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    
+    
+    
+    );
+
+CREATE Table products (
+    
+    
+    id BIGSERIAL PRIMARY KEY,
+
+
+
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    
+    
+    );
+
+CREATE Table invoices (
+    
+    
+    id BIGSERIAL PRIMARY KEY,
+
+
+
+
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    
+    
+    
+    );
+
+CREATE Table invoice_products (
+    
+    
+    
+    id BIGSERIAL PRIMARY KEY,
+
+
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    
+    
+    
+    );

7c3d2a35dc4ee14faea9d6a5650f47b16e6ca07d
Anik Mondol <anik(558363)@gmail.com>
2026-08-17 00:51:03 +0600

    Add SQL procedure for stock transaction management with decrease and increase functionality

--- class-73.sql
+++ class-73.sql
@@ -17,7 +17,28 @@ Update -- Action 3
 
 
 CREATE OR REPLACE PROCEDURE trans_stock(
-    sourch_id INT,
-    destination_id INT,
-    q
+    sourch_id INT, ---> stock decrease
+    destination_id INT, ---> stock increase
+    quantity INT  ---> stock quantity
 )
+AS $$
+
+BEGIN
+
+    --- stock decrease
+    UPDATE products set qty = qty - quantity WHERE id = sourch_id;
+
+
+    --- stock increase
+    UPDATE products set qty = qty + quantity WHERE id = destination_id;
+
+    -- commit
+    COMMIT;
+
+
+END; 
+$$ LANGUAGE plpgsql;
+
+
+
+CALL trans_stock(3, 1, 70);

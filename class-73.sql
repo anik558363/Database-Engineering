4b607beb723d96abca6e2c37f14e5bf66891d426
Anik Mondol <anik(558363)@gmail.com>
2026-08-17 00:26:57 +0600

    Add SQL procedures for number printing and stock transactions; create CSS for desktop layout and update HTML structure with navigation

---
+++ class-73.sql
@@ -0,0 +1,23 @@
+/*
+
+Multi Action
+
+
+Update -- Action 1
+Create -- Action 2
+Delete -- Action 2
+
+
+Update -- Action 1
+Update -- Action 2
+Update -- Action 3
+
+*/
+
+
+
+CREATE OR REPLACE PROCEDURE trans_stock(
+    sourch_id INT,
+    destination_id INT,
+    q
+)
